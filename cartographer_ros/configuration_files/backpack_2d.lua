eabf1353e426e840c019f2f8e45c353c517d272f
thomasd <tdai@hnrcorp.com>
2016-10-20 15:59:00 +0800

    change to odom

--- cartographer_ros/configuration_files/backpack_2d.lua
+++ cartographer_ros/configuration_files/backpack_2d.lua
@@ -19,16 +19,16 @@ options = {
   map_frame = "map",
   tracking_frame = "base_link",
   published_frame = "base_link",
-  odom_frame = "odom",
+  odom_frame = "base_odom",
   provide_odom_frame = true,
-  use_odometry_data = false,
+  use_odometry_data = true,
   use_constant_odometry_variance = false,
   constant_odometry_translational_variance = 0.,
   constant_odometry_rotational_variance = 0.,
-  use_horizontal_laser = false,
-  use_horizontal_multi_echo_laser = true,
-  horizontal_laser_min_range = 0.,
-  horizontal_laser_max_range = 30.,
+  use_horizontal_laser = true,
+  use_horizontal_multi_echo_laser = false,
+  horizontal_laser_min_range = 0.05,
+  horizontal_laser_max_range = 10.,
   horizontal_laser_missing_echo_ray_length = 5.,
   num_lasers_3d = 0,
   lookup_transform_timeout_sec = 0.2,
@@ -36,12 +36,13 @@ options = {
   pose_publish_period_sec = 5e-3,
 }
 
--- MAP_BUILDER.use_trajectory_builder_2d = true
+--MAP_BUILDER.use_trajectory_builder_2d = true
 MAP_BUILDER.use_trajectory_builder_2d = true
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 TRAJECTORY_BUILDER_2D.use_imu_data = false
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.15
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(35.)
 SPARSE_POSE_GRAPH.optimization_problem.huber_scale = 1e2
-
 return options
+
+-- rosbag filter L4.bag filter.bag "topic == '/scan' or topic == '/tf' and m.transforms[0].header.frame_id != 'map'"
